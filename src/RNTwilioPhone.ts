a847812af98eb84c0817ea59beffe6ca792c8feb
alex choi <heyalexchoi@gmail.com>
2022-07-21 13:06:12 -0400

    added type annotations

--- src/RNTwilioPhone.ts
+++ src/RNTwilioPhone.ts
@@ -9,7 +9,18 @@ import {
 import VoipPushNotification from 'react-native-voip-push-notification';
 import ramdomUuid from 'uuid-random';
 
-let messaging
+type callback = (param: any) => any;
+
+interface FirebaseMessagingModule {
+  (): FirebaseMessagingModule;
+  setBackgroundMessageHandler: (handler: callback) => any;
+  getToken: () => Promise<any>;
+  onTokenRefresh: (handler: callback) => any;
+  onMessage: (handler: callback) => any;
+};
+
+let messaging: FirebaseMessagingModule;
+
 if (Platform.OS === 'android') {
   messaging = require('@react-native-firebase/messaging')
 }
